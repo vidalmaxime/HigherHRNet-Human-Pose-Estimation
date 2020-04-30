928c257f7490e7711bd43f0fc7b5a22fe3cdd5e1
Maxime Vidal <mvidal@student.ethz.ch>
2020-04-30 12:08:55 +0200

    changed results keypoint saving

--- lib/dataset/COCODataset.py
+++ lib/dataset/COCODataset.py
@@ -255,7 +255,7 @@ class CocoDataset(Dataset):
         cat_id = data_pack['cat_id']
         keypoints = data_pack['keypoints']
         cat_results = []
-        num_joints = 17
+        num_joints = 20
 
         for img_kpts in keypoints:
             if len(img_kpts) == 0:
