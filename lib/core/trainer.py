671fd7f2ccb7db47193e7b7ab76ca2e4f3e62e49
Maxime Vidal <mvidal@student.ethz.ch>
2020-05-05 15:23:55 +0200

    minor fix

--- lib/core/trainer.py
+++ lib/core/trainer.py
@@ -53,8 +53,8 @@ def do_train(cfg, model, data_loader, loss_factory, optimizer, epoch,
         mse_loss = JointsMSELoss(
             use_target_weight=False
         ).cuda()
-        logger.info(len(outputs[1]))
-        logger.info(len(heatmaps[1]))
+        logger.info(outputs[1].size(0))
+        logger.info(outputs[1].size(1))
         mse_losses = mse_loss(outputs[1], heatmaps[1], masks)
         mse_loss_meter.update(mse_losses.item(), images.size(0))
 
