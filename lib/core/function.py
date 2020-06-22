38e924e7459e7f8ae1334a13678096aed28bb1ad
Maxime Vidal <mvidal@student.ethz.ch>
2020-06-22 22:05:30 +0200

    hotfix rmse function

--- lib/core/function.py
+++ lib/core/function.py
@@ -177,7 +177,7 @@ def validate(config, val_loader, val_dataset, model, output_dir,
                             npgt[1::3] * [maskgt] - npdt[1::3] * [maskgt]) ** 2)
                 RMSE_pcutoff01 = RMSE[0][mask01]
                 RMSE_pcutoff06 = RMSE[0][mask06]
-                mean_rmse = np.round(np.nanmean(RMSE[0])/np.nansum(maskgt), 2)
+                mean_rmse = np.round(np.nansum(RMSE[0])/np.nansum(maskgt), 2)
                 mean_rmse_pcutoff01 = np.nanmean(RMSE_pcutoff01.flatten())
                 mean_rmse_pcutoff06 = np.nanmean(RMSE_pcutoff06.flatten())
                 mean_rmse_list.append(mean_rmse)
